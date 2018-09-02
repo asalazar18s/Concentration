9bad509539adb01371ba886937224d44f90fddc0
Aaron Salazar <AS@aaron.local>
2018-09-01 19:42:50 -0500

    Auto Layout
    Auto Layout cchanges and refactorin

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,10 +10,30 @@ import Foundation
 
 class Concentration
 {
-    var cards = [Card]()
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private(set) var cards = [Card]()
+    private  var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp{
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set{
+            for index in cards.indices{
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "concentratrion.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 // check if cards Match
@@ -22,18 +42,14 @@ class Concentration
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             } else {
-                // either no cards
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
     }
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "concentratrion.init(\(numberOfPairsOfCards)): at least one pàir of cards")
+
         for _ in 0..<numberOfPairsOfCards {
             let card = Card()
             cards.append(card)
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count+1) / 2
+    }
     
     var flipCount = 0 { didSet{ flipCountLabel.text = "Flips: \(flipCount)"}}
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
     
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -58,19 +62,29 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["🎃","👻","🍩","🍭", "🍪", "🍏","🦇"]
+   private var  emojiChoices = ["🎃","👻","🍩","🍭", "🍪", "🍏","🦇"]
     
-    var emoji = [Int:String]()
+    private var  emoji = [Int:String]()
 
     
-    func emoji(for card: Card) -> String{
+    private func emoji(for card: Card) -> String{
         if emoji[card.identifier] == nil, emojiChoices.count > 0{
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
-            
-            
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?" // handling the same as top code
     }
 }
 
+extension Int{
+    var arc4random: Int {
+        if self > 0 {
+        return Int(arc4random_uniform(UInt32(self)))
+        }
+        else if self < 0{
+            return -Int(arc4random_uniform(UInt32(self)))
+        }
+        else {
+            return 0
+        }
+    }
+}
